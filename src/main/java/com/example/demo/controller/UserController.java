1945d1a81bc2273f20401e959450a64ddd89e412
songyi <nse1005@gmail.com>
2024-10-15 14:49:59 +0900

    feat: GET 방식으로 사번을 리턴하는 API 생성

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/user")
 public class UserController {
-    @GetMapping("/user")
-    public ResponseEntity<?> user(){
+    @GetMapping()
+    public ResponseEntity<?> getUser(){
         return ResponseEntity.ok("82212582");
     }
 }
